bf27393b946a169f835ba541229f62b37f2ba0b5
hayeong120 <jbg01200249@gmail.com>
2024-02-21 23:28:28 +0900

    Chapter 03-5 논리 연산자

--- Chapter_03/src/OperatorEx24.java
+++ Chapter_03/src/OperatorEx24.java
@@ -4,7 +4,7 @@ public class OperatorEx24 {
 	public static void main(String[] args) {
 
 		int x = 0;
-		char ch = ' ';
+		char ch = ' '; 
 		
 		x = 15;
 		System.out.printf("x=%2d, 10 < x && x < 20 = %b\n", x, 10 < x && x < 20);
